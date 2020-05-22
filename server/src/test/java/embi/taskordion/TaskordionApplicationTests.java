cdaa437ce59df9ebffa4029481337e2bc9acb2db
Balazs Molnar <balazs.molnar@softwarepark.cc>
2020-05-22 14:17:15 +0200

    chore: remove .net version, create spring boot version

---
+++ server/src/test/java/embi/taskordion/TaskordionApplicationTests.java
@@ -0,0 +1,13 @@
+package embi.taskordion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskordionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
