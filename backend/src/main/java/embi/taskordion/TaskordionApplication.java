767b2ac5965290fce891f802ace7daef7a06e69b
Balazs Molnar <balazs.molnar@softwarepark.cc>
2020-06-19 18:05:40 +0200

    chore: add backend

---
+++ backend/src/main/java/embi/taskordion/TaskordionApplication.java
@@ -0,0 +1,13 @@
+package embi.taskordion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskordionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskordionApplication.class, args);
+	}
+
+}
