8ec2c269e0065a5100aea151dc23df25a302db19
Balazs Molnar <balazs.molnar@softwarepark.cc>
2020-07-05 16:51:40 +0200

    chore: set up proxy for dev

--- backend/src/main/java/embi/taskordion/TaskordionApplication.java
+++ backend/src/main/java/embi/taskordion/TaskordionApplication.java
@@ -2,6 +2,10 @@ package embi.taskordion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TaskordionApplication {
@@ -9,5 +13,4 @@ public class TaskordionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskordionApplication.class, args);
 	}
-
 }
